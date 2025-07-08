4c74829ce1ef1b0dbe665c868c8b988b7726b353
DongGyun <wooheuy5752@gmail.com>
2025-07-08 20:50:05 +0900

    chore: eslint 및 prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,60 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import eslintPluginImport from 'eslint-plugin-import'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      'unused-imports': eslintPluginUnusedImports,
+      import: eslintPluginImport,
+      'simple-import-sort': eslintPluginSimpleImportSort,
+      prettier: eslintPluginPrettier,
+      '@typescript-eslint': typescriptEslint,
+    },
+  },
+  {
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'import/newline-after-import': 'warn',
+      'import/no-named-as-default': 'off',
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            ['^react', '^@?\\w'],
+            ['^@/(.*)'],
+            ['^\\.\\.(?!/?$)', '^\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
